bc14b0ab041cc40f4b5f567de6f7809a60c56b9a
CodeGunner100a <codegunner178@gmail.com>
2026-06-10 09:30:05 -0400

    Update Ejercicio3.pas

--- Ejercicio3.pas
+++ Ejercicio3.pas
@@ -1,6 +1,38 @@
-//EJERCICIO 3
+//EJERCICIO 3 cadenas en archivo cadenas
 
+function Ejercicio3(cad: String): String;
+var
+  i, j: Integer;
+  palabra, invertida, salida: String;
+begin
+  salida := '';
+  palabra := '';
+
+  cad := cad + ' ';
+
+  for i := 1 to length(cad) do
+  begin
+    if cad[i] <> ' ' then
+      palabra := palabra + cad[i]
+    else
+    begin
+      invertida := '';
+
+      for j := length(palabra) downto 1 do
+        invertida := invertida + palabra[j];
+
+      salida := salida + invertida + ' ';
+      palabra := '';
+    end;
+  end;
+
+  Result := salida;
+end;
 
 
 
 //LLAMADA
+Edit2.Text := (Ejercicio3(cad));
+
+
+
